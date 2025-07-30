ddcfdb40d3e28eb47965ba4e442e02b658e163ec
Stu <lazylinepainterstuart@gmail.com>
2025-07-30 12:17:21 +0100

    [0] Initial commit of supplied source file
    Simply the starting point

---
+++ GK/Speaker.cs
@@ -0,0 +1,170 @@
+﻿using GK.Abstractions;
+using GK.Models;
+
+namespace GK.Talks
+{
+	/// <summary>
+	/// Represents a single speaker
+	/// </summary>
+	/// </summary>
+	public class Speaker
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public int? Exp { get; set; }
+		public bool HasBlog { get; set; }
+		public string BlogURL { get; set; }
+		public WebBrowser Browser { get; set; }
+		public List<string> Certifications { get; set; }
+		public string Employer { get; set; }
+		public int RegistrationFee { get; set; }
+		public List<Session> Sessions { get; set; }
+
+		/// <summary>
+		/// Register a speaker
+		/// Prams 
+		/// strFirstName speakers first name
+		///	strLastName ^^^ last name
+		/// Email the email
+		/// blogs etc.....
+		/// </summary>
+		/// <returns>speakerID</returns>
+		public RegisterResponse Register(IRepository repository, string strFirstName, String strLastName, string Email, int iExp, Boolean BHasBlog, string URL, string strBrowser, string csvCertifications, String s_Emp, int iFee, string csvSess)
+		{
+			// lets init some vars
+			int? speakerId = null;
+			bool good = false;
+			bool appr = false;
+			//var nt = new List<string> {"Node.js", "Docker"};
+			var ot = new List<string>() { "Cobol", "Punch Cards", "Commodore", "VBScript" };
+
+			//DEFECT #5274 CL 12/10/2010
+			//We weren't filtering out the prodigy domain so I added it.
+			var domains = new List<string>() { "aol.com", "prodigy.com", "compuserve.com" };
+
+			if (!((FirstName == null) || (FirstName.Length == 0)))
+			{
+				if (!((LastName == null) || (LastName.Length == 0))) 
+				{
+					if (!((Email == null) || (Email.Length == 0)))
+					{
+						//put list of employers in array
+						var emps = new List<string>() { "Pluralsight", "Microsoft", "Google" };
+
+						good = Exp > 10 || HasBlog || Certifications.Count() > 3 || emps.Contains(Employer);
+
+						if (!good)
+						{
+							//need to get just the domain from the email
+							string emailDomain = Email.Split('@').Last();
+
+							if (!domains.Contains(emailDomain) && (!(Browser.Name == WebBrowser.BrowserName.InternetExplorer && Browser.MajorVersion < 9)))
+							{
+								good = true;
+							}
+						}
+
+						if (good)
+						{
+							if (Sessions.Count() != 0)
+							{
+								foreach (var session in Sessions)
+								{
+									//foreach (var tech in nt)
+									//{
+									//    if (session.Title.Contains(tech))
+									//    {
+									//        session.Approved = true;
+									//        break;
+									//    }
+									//}
+
+									foreach (var tech in ot)
+									{
+										if (session.Title.Contains(tech) || session.Description.Contains(tech))
+										{
+											session.Approved = false;
+											break;
+										}
+										else
+										{
+											session.Approved = true;
+											appr = true;
+										}
+									}
+								}
+							}
+							else
+							{
+								return new RegisterResponse(RegisterError.NoSessionsProvided);
+							}
+
+							if (appr)
+							{
+								//if we got this far, the speaker is approved
+								//let's go ahead and register him/her now.
+								//First, let's calculate the registration fee. 
+								//More experienced speakers pay a lower fee.
+								if (Exp <= 1)
+								{
+									RegistrationFee = 500;
+								}
+								else if (Exp >= 2 && Exp <= 3)
+								{
+									RegistrationFee = 250;
+								}
+								else if (Exp >= 4 && Exp <= 5)
+								{
+									RegistrationFee = 100;
+								}
+								else if (Exp >= 6 && Exp <= 9)
+								{
+									RegistrationFee = 50;
+								}
+								else
+								{
+									RegistrationFee = 0;
+								}
+
+
+								//Now, save the speaker and sessions to the db.
+								try
+								{
+									speakerId = repository.SaveSpeaker(this);
+								}
+								catch (Exception e)
+								{
+									//in case the db call fails 
+								}
+							}
+							else
+							{
+								return new RegisterResponse(RegisterError.NoSessionsApproved);
+							}
+						}
+						else
+						{
+							return new RegisterResponse(RegisterError.SpeakerDoesNotMeetStandards);
+						}
+					}
+					else
+					{
+						return new RegisterResponse(RegisterError.EmailRequired);
+					}
+				}
+				else
+				{
+					return new RegisterResponse(RegisterError.LastNameRequired);
+				}
+			}
+			else
+			{
+				return new RegisterResponse(RegisterError.FirstNameRequired);
+			}
+
+			//if we got this far, the speaker is registered.
+			return new RegisterResponse((int)speakerId);
+		}
+	}
+}
